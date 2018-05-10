50dbb2fcdacb1d8b7761e20f6f8c220c8621689c
sumarsanaadi <sumarsana.adi@gmail.com>
2018-05-10 18:53:22 +0700

    add exec damage calc sumarsanaAdi

---
+++ exercise-5-sumarsanaadi.js
@@ -0,0 +1,33 @@
+//Exercise 5 Logic Challenge: Damage Calculation
+
+/*
+Problem
+Diberikan function attack(), damageCalculation().
+
+Function damageCalculation() akan menerima 2 parameter yaitu numberOfAttacks dan damagePerAttack
+Function attack() akan menerima 1 parameter yaitu damage
+[IMPLEMENTASI] Gunakan function damageCalculation() untuk menghitung damage yang diterima dan gunakan function attack() untuk mensimulasikan setiap attack dengan rumus damage - 2 disetiap attack.
+
+[CONTOH] damageCalculation(3, 10) akan mengembalikan nilai 24
+
+Karena attack akan dikurangi 2, maka setiap attack akan menghasilkan damage 8
+*/
+
+function attack (damage) {
+  return damage -2;
+}
+
+function damageCalculation (numberOfAttacks, damagePerAttack) {
+  var totalDamage = 0;
+  for (var i = 0; i < numberOfAttacks; i++) {
+    totalDamage += attack(damagePerAttack);
+  }
+  return totalDamage;
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
