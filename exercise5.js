904a649733733dac02222c82312657b0e080b9d9
taqiabdulaziz <svx.annexiv@gmail.com>
2018-10-08 22:46:04 +0700

    add exercise 5 week 5

---
+++ exercise5.js
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+//TAQI A
+function attack(damage) {
+  return damage - 2;
+}
+
+function damageCalculation(numberOfAttacks, damagePerAttack) {
+  return attack(damagePerAttack) * numberOfAttacks;
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
