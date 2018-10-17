78cf774a75ff26a515c452fe28918f5a65184005
Kevin Wijaya <Kevin@Kevins-MacBook-Pro.local>
2018-10-17 21:43:32 +0700

    baru coba upload ex 5

---
+++ excercise-5.js
@@ -0,0 +1,17 @@
+function attack (damage) {
+    return damage - 2;
+  }
+  
+  function damageCalculation (numberOfAttacks, damagePerAttack) {
+   result =  attack(damagePerAttack) * numberOfAttacks;
+
+   return result
+
+  }
+  
+  // TEST CASE
+  console.log(damageCalculation(9, 25)); // 207
+  
+  console.log(damageCalculation(10, 4)); // 20
+  
+  console.log(damageCalculation(5, 20)); // 90
