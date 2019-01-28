929f24c198b7351e6f9b8f3d74dd5153c909354e
mrifkyilyas <muhammadrifkyilyas@gmail.com>
2019-01-28 12:04:05 +0700

    upload exercise 5

---
+++ exercise5.js
@@ -0,0 +1,19 @@
+function attack (damage) {
+    damage = damage - 2 
+  return damage
+}
+
+function damageCalculation (numberOfAttacks, damagePerAttack) {
+  damage = numberOfAttacks*attack(damagePerAttack)
+  return damage
+}
+
+
+
+// TEST CASE
+
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
