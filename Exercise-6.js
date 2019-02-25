9f7970f083612154e60fd5b3008940c850ce40d3
mhmmd-ysf <muhammad_yusuf22@outlook.co.id>
2019-02-25 10:59:57 +0700

    Commit A6 Damage Calculation

---
+++ Exercise-6.js
@@ -0,0 +1,17 @@
+function attack(damage) {
+	// Code disini
+	return damage - 2
+}
+
+function damageCalculation(numberOfAttacks, damagePerAttack) {
+	// Code disini
+	var damage = attack(damagePerAttack)
+	return damage * numberOfAttacks
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
