d05cc11e00de57b6191271656d2b5dc2b955c56b
gerintuanakotta <gerintuanakotta@gmail.com>
2019-02-25 11:12:49 +0700

    uploaded

---
+++ p0_w5_exc06.js
@@ -0,0 +1,22 @@
+//Gerin Prapaska
+//HACKTIV8 Batch 29, Classic Fox
+//Phase 0, Week 5
+//Exercises 6: 
+//---------------------------------------
+
+function attack (damage) {
+  // Code disini
+  return damage-2
+}
+
+function damageCalculation (numberOfAttacks, damagePerAttack) {
+  // Code disini
+  return numberOfAttacks*attack(damagePerAttack)
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
