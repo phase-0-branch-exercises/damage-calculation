88cc1e3326da7671a346b0b1ce95e2084abe19c8
azputra <ahmadzp102@gmail.com>
2019-07-29 12:17:56 +0700

    tambahkan damageCalculation

---
+++ exercise-5.js
@@ -0,0 +1,21 @@
+function attack(damage) {
+    // Code disini
+    return damage - 2;
+  }
+  
+  function damageCalculation(numberOfAttacks, damagePerAttack) {
+    // Code disini
+    var tempDemage = 0;
+    var serang = attack(damagePerAttack);
+    for (var i = 0; i < numberOfAttacks; i++) {
+      tempDemage += serang;
+    }
+    return tempDemage;
+  }
+  
+  // TEST CASE
+  console.log(damageCalculation(9, 25)); // 207
+  
+  console.log(damageCalculation(10, 4)); // 20
+  
+  console.log(damageCalculation(5, 20)); // 90
