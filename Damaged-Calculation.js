3885b8ab63484883374add90268850f6b617ae97
laskarks <laskar.ksatria12@gmail.com>
2019-07-31 06:28:07 -0400

    Tugas Damage-calculation Week 5

---
+++ Damaged-Calculation.js
@@ -0,0 +1,35 @@
+// Logic Challenge: Damage Calculation
+// Diberikan function attack(), damageCalculation().
+
+// Function damageCalculation() akan menerima 2 parameter yaitu numberOfAttacks dan damagePerAttack
+// Function attack() akan menerima 1 parameter yaitu damage
+// [IMPLEMENTASI] Gunakan function damageCalculation() untuk menghitung damage yang diterima dan gunakan function attack() untuk mensimulasikan 
+// setiap attack dengan rumus damage - 2 disetiap attack.
+
+// [CONTOH] damageCalculation(3, 10) akan mengembalikan nilai 24
+
+// Karena attack akan dikurangi 2, maka setiap attack akan menghasilkan damage 8
+
+function attack(damage) {
+
+    let minus = 2;
+    let total = damage - minus;
+    return total
+
+}
+
+function damageCalculation(numberOfAttacks, damagePerAttack) {
+
+    let damag = attack(damagePerAttack);
+    let sumDamage = damag * numberOfAttacks;
+    return sumDamage
+
+
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
