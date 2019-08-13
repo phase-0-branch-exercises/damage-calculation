2671f302e252b1ad15adf786e3dc11b9b533cf61
Kevin Anantha <kevin.anantha@gmail.com>
2019-08-13 23:49:12 +0700

    :rocket:

--- attack.js
+++ attack.js
@@ -8,7 +8,5 @@ function damageCalculation (numberOfAttacks, damagePerAttack) {
 
 // TEST CASE
 console.log(damageCalculation(9, 25)); // 207
-
 console.log(damageCalculation(10, 4)); // 20
-
 console.log(damageCalculation(5, 20)); // 90
