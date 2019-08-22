9ae74d1336de4e5bc2f0337a0c621efc5def9ca4
Panji SN <panji1595@gmail.com>
2019-08-22 12:38:48 +0700

    adding conditional

--- Damage-Calculation-exercise.js
+++ Damage-Calculation-exercise.js
@@ -6,6 +6,10 @@ function damageCalculation (numberOfAttacks, damagePerAttack) {
     //var calc = 0;
     if (numberOfAttacks == 1 ){
       return attack(damagePerAttack);
+    } else if (numberOfAttacks < 1) {
+      return "Jumlah serangan harus lebih besar daripada 0";
+    } else if (damagePerAttack < 2) {
+      return "Nilai serangan harus lebih besar daripada 2"
     } else {
       return attack(damagePerAttack) + damageCalculation(numberOfAttacks -1, damagePerAttack);
     }
@@ -17,3 +21,9 @@ console.log(damageCalculation(9, 25)); // 207
 console.log(damageCalculation(10, 4)); // 20
   
 console.log(damageCalculation(5, 20)); // 90
+
+console.log(damageCalculation(0, 20)); // 1
+
+console.log(damageCalculation(5, 2)); // 0
+
+console.log(damageCalculation(2, 1)); // 90
