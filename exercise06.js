6e5b573ec63c242e75742d1aa33182872664093b
edycakra <edycakrasinaga@gmail.com>
2019-09-20 21:30:46 +0700

    add task damage-calculation

---
+++ exercise06.js
@@ -0,0 +1,19 @@
+function attack (damage) {
+    // Code disini
+    damage -= 2;
+    return damage;
+    
+  }
+  
+  function damageCalculation (numberOfAttacks, damagePerAttack) {
+    // Code disini
+    var realdamage = attack(damagePerAttack);
+    return numberOfAttacks*realdamage;
+  }
+  
+  // TEST CASE
+  console.log(damageCalculation(9, 25)); // 207
+  
+  console.log(damageCalculation(10, 4)); // 20
+  
+  console.log(damageCalculation(5, 20)); // 90
