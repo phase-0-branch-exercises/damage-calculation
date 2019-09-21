db1dd8bb747fffcda0d66553bb8711e980f9fbe3
ujangaja <ujangaja@gmail.com>
2019-09-21 18:48:52 +0700

    tugas damageCalculation

---
+++ damageCalculation.js
@@ -0,0 +1,50 @@
+/* 
+Logic Challenge: Damage Calculation
+
+Diberikan function attack(), damageCalculation().
+
+    Function damageCalculation() akan menerima 2 parameter yaitu numberOfAttacks dan damagePerAttack
+    Function attack() akan menerima 1 parameter yaitu damage
+
+[IMPLEMENTASI] Gunakan function damageCalculation() untuk menghitung damage yang diterima dan gunakan function attack() untuk mensimulasikan setiap attack dengan rumus damage - 2 disetiap attack.
+
+[CONTOH] damageCalculation(3, 10) akan mengembalikan nilai 24
+
+Karena attack akan dikurangi 2, maka setiap attack akan menghasilkan damage 8
+
+function attack (damage) {
+  // Code disini
+}
+
+function damageCalculation (numberOfAttacks, damagePerAttack) {
+  // Code disini
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
+
+*/
+function damageCalculation(numberOfAttacks, damagePerAttack) {
+    // Code disini
+    var perAttack = attack(damagePerAttack)
+    var hasil = numberOfAttacks * perAttack
+
+    return hasil
+
+}
+
+function attack(damage) {
+    return damage - 2
+
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
