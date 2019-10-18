38bdb76839e3c642387b2d89bf5ca825c565328a
mhmmd-ysf <muhammad_yusuf22@outlook.co.id>
2019-10-18 13:45:22 +0700

    jangan wkwkwk

---
+++ damage-calculation.js
@@ -0,0 +1,15 @@
+function attack(damage) {
+  // Code disini
+  // Code tambahan
+}
+
+function damageCalculation(numberOfAttacks, damagePerAttack) {
+  // Code disini
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
