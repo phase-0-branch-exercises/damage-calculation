e7b288cd865f3feb8c294179c85d407ecd810b11
fadhilahrayafi <fadhilahrayafi@gmail.com>
2019-11-17 09:13:45 +0700

    add file damage

---
+++ damageCal.js
@@ -0,0 +1,17 @@
+function attack(damage) {
+  // Code disini
+  return damage - 2;
+}
+
+function damageCalculation(numberOfAttacks, damagePerAttack) {
+  // Code disini
+  result = attack(damagePerAttack);
+  return numberOfAttacks * result;
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
