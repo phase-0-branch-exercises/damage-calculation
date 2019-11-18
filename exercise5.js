74ac9cc57624d5176ec6f636b01bb1bf99ad3a02
MickeyStrike <irfanmaulana281299@gmail.com>
2019-11-18 10:27:22 +0700

    Add Tugas

---
+++ exercise5.js
@@ -0,0 +1,14 @@
+function attack(damage){
+    return damage-2
+}
+
+function damageCalculation(numberOfAttacks, damagePerAttack){
+    if(numberOfAttacks===1){
+        return attack(damagePerAttack);
+    }
+    return attack(damagePerAttack) + damageCalculation(numberOfAttacks-1,damagePerAttack)
+}
+
+console.log(damageCalculation(9, 25)); // 207
+console.log(damageCalculation(10, 4)); // 20
+console.log(damageCalculation(5, 20)); // 90
