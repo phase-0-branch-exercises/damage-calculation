7f13cd5cb9c418e5072663f48dab612c15684117
MuhammadFahmiRidwan <mfahmiridwan50@gmail.com>
2019-12-13 14:17:56 +0700

    kaleng

---
+++ 5.DamageCalculation.js
@@ -0,0 +1,38 @@
+// Diberikan function attack(), damageCalculation().
+// 
+// Function damageCalculation()akan menerima 2 parameter yaitu numberOfAttacks dan damagePerAttack
+// Function attack()akan menerima 1 parameter yaitu damage
+// [IMPLEMENTASI] Gunakan function damageCalculation()untuk menghitung damage yang diterima dan gunakan function attack()untuk mensimulasikan setiap attack dengan rumus damage - 2disetiap attack.
+// 
+// [CONTOH] damageCalculation(3, 10)akan mengembalikan nilai 24
+// 
+// Karena attack akan dikurangi 2, maka setiap attack akan menghasilkan damage 8
+
+function attack (damage) {
+    // Code disini
+    if (damage === 0) {
+      return 0
+    }
+    else {
+      return temp + attack(damage-1)
+    }
+  }
+  console.log(attack(10))
+  
+  function damageCalculation (numberOfAttacks, damagePerAttack) {
+    // Code disini
+    let result = numberOfAttacks * damagePerAttack
+    if (numberOfAttacks == 0) {
+      return 0
+    }
+    else {
+      return result - attack(numberOfAttacks)
+    }
+  }
+  
+  // TEST CASE
+  console.log(damageCalculation(9, 25)); // 207
+  
+  console.log(damageCalculation(10, 4)); // 20
+  
+  console.log(damageCalculation(5, 20)); // 90
