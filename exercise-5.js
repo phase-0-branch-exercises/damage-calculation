f764f1a195185340e7ae59a6c84d001d3882cecc
fajrinnr <fajrin.noorrachman11@gmail.comgit config --global user.name fajrinnrgitgit config --global user.email fajrin.noorrachman11@gmail.com>
2019-12-13 14:17:24 +0700

    Tugas

--- exercise-5.js
+++ exercise-5.js
@@ -1,16 +1,15 @@
-function attack (damage) {
-    let dpa = damage - 2
-    return dpa
-  }
-  
-  function damageCalculation (numberOfAttacks, damagePerAttack) {
-    var dmg = attack(damagePerAttack)
-    return dmg * numberOfAttacks
-  }
-  
-  // TEST CASE
-  console.log(damageCalculation(9, 25)); // 207
-  
-  console.log(damageCalculation(10, 4)); // 20
-  
-  console.log(damageCalculation(5, 20)); // 90
+function attack(damage) {
+  let dpa = damage - 2
+  return dpa
+}
+
+function damageCalculation(numberOfAttacks, damagePerAttack) {
+  var dmg = attack(damagePerAttack)
+  return dmg * numberOfAttacks
+}
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
