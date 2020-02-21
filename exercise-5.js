e2855e26d01bf5620428a2964214d895b3fab82d
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-02-21 16:45:25 +0700

    tugas week 5

---
+++ exercise-5.js
@@ -0,0 +1,16 @@
+function attack (damage) {
+  // Code disini
+  return damage - 2
+}
+
+function damageCalculation (numberOfAttacks, damagePerAttack) {
+  // Code disini
+  return numberOfAttacks * attack(damagePerAttack)
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
