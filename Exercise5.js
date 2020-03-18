31f8bd808815cd609abdcdd7bd7c69c7ca2430c4
alamrumambi <alam.abumaryam@gmail.com>
2020-03-18 11:30:32 +0700

    upload Exercise5.js

---
+++ Exercise5.js
@@ -0,0 +1,16 @@
+function attack (damage) {
+    return damage - 2;
+    // Code disini
+  }
+  
+  function damageCalculation (numberOfAttacks, damagePerAttack) {
+    return attack(damagePerAttack) * numberOfAttacks;
+    // Code disini
+  }
+  
+  // TEST CASE
+  console.log(damageCalculation(9, 25)); // 207
+  
+  console.log(damageCalculation(10, 4)); // 20
+  
+  console.log(damageCalculation(5, 20)); // 90
