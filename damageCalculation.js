902bcdf358e66d05059acfe951ad3fb86f4403a8
dendyeka <dendyekap@gmail.com>
2020-03-21 22:17:06 +0700

    add damageCalculation.js

---
+++ damageCalculation.js
@@ -0,0 +1,18 @@
+function attack (damage) {
+  return damage-2
+}
+
+function damageCalculation (numberOfAttacks, damagePerAttack) {
+  if (numberOfAttacks === 0) {
+    return 0
+  }
+
+  return attack(damagePerAttack) + damageCalculation(numberOfAttacks-1, damagePerAttack)
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
