125140853a774b805ed0485617d65ebf8931e5a2
Sujackson <su_jackson99@yahoo.com>
2020-03-21 11:36:18 +0700

    add damageCalculation.js

---
+++ damageCalculation.js
@@ -0,0 +1,21 @@
+function attack (damage) {
+    // Code disini
+    return damage-2
+  }
+  
+  function damageCalculation (numberOfAttacks, damagePerAttack) {
+    // Code disini
+    var total=0
+    for (var i = 0; i < numberOfAttacks; i++) {
+        var damage=attack(damagePerAttack)
+        total+=damage
+    }
+    return total
+  }
+  
+  // TEST CASE
+  console.log(damageCalculation(9, 25)); // 207
+  
+  console.log(damageCalculation(10, 4)); // 20
+  
+  console.log(damageCalculation(5, 20)); // 90
